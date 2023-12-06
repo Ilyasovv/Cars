8ca836eb5aa324e1906486ae85e9569c63557c4f
MisterAbai <abaiilyasovv@gmail.com>
2023-12-06 22:20:22 +0500

    foto

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ function setScrollBy() {
   window.scrollBy(0, 695);
   const winSCrBy = window.pageXOffset;
 }
+function e() {
+  window.scrollBy(0, 1000);
+  const winSRlBy = window.pageXOffset;
+}
+const click = document.getElementById("menu-click");
+click.addEventListener("click", e);
